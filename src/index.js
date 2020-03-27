45c93b05c79cc9c2aafc3ce591882289c5d0303a
EunHyeYun <lillyeh@hatiolab.com>
2020-03-27 10:56:07 +0900

    탭 이벤트 처리를 위해서 board-viewer를 사용하여 integrator화면 생성하도록 변경

--- src/index.js
+++ src/index.js
@@ -1,43 +1,8 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
-import 'url-polyfill'
-import 'url-search-params-polyfill'
 import { SceneIntegrator } from '@things-scene/things-board-integration'
+import { SceneProvider } from '@things-scene/things-board-integration'
+import { BoardViewer } from '@things-scene/things-board-integration'
+export { SceneIntegrator, SceneProvider, BoardViewer }
 
-export function init({ baseURL, queryString, authorizationKey, license = '' }) {
-  var fit = 'both' // 'both', 'ratio', 'center', 'none'
 
-  var searchParams
-
-  if (!queryString) {
-    return alert('Parameter가 존재하지 않습니다.')
-  }
-
-  searchParams = new URLSearchParams(queryString)
-
-  var sceneName = searchParams.get('scene')
-
-  if (!sceneName) {
-    return alert('Scene name이 필요합니다.')
-  }
-
-  authorizationKey = authorizationKey;
-
-  var integrator = SceneIntegrator.instance({
-    authorizationKey,
-    withCredentials: true,
-    baseURL,
-    fit,
-    license
-  })
-
-  integrator.integrate({
-    target: 'scene-viewer',
-    sceneName: sceneName,
-    callback: board => {
-      window.board = board
-
-      board.root.invalidate()
-    }
-  })
-}
